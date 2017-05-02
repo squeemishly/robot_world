b77accad1e9aac9e43e358844062de804b50e6e6
squeemishly <squeemishly@gmail.com>
2017-05-02 16:00:46 -0600

    trying to add a robot about to work on adding a database

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,13 +1,27 @@
 require_relative '../models/robot.rb'
+require 'pry'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  not_found do
+    erb :error
+  end
+
   get '/robots' do
+    @robots = Robot.all
+
     erb :index
   end
 
   get '/robots/new' do
     erb :new_robot
+  end
+
+  post '/robots' do
+    robot = Robot.new(params)
+    robot.save
+    binding.pry
+    erb :index
   end
 end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -4,5 +4,17 @@ class Robot
   def initialize(robot_params)
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @name = robot_params[:name]
+    @city = robot_params[:city]
+    @state = robot_params[:state]
+    @department = robot_params[:department]
+  end
+
+  def self.all
+    
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?);", @name, @city, @state, @department)
   end
 end
